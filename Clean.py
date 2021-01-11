ba40744b54c1a752cf1a87a51d3b934f222f80f3
Rohan Ben Joseph <rohan.benjoseph@gmail.com>
2021-01-11 14:26:25 +0000

    Update Clean.py

--- Clean.py
+++ Clean.py
@@ -1,5 +1,6 @@
 import pandas as panda
 import openpyxl
+# Combines Sheets of Creators into a single, Alphabetically organized list 
 
 # Create a Pandas Excel writer using XlsxWriter as the engine.
 writer = panda.ExcelWriter('demo.xlsx', engine='xlsxwriter')
@@ -7,6 +8,8 @@ writer = panda.ExcelWriter('demo.xlsx', engine='xlsxwriter')
 df = panda.concat(panda.read_excel('demo.xlsx', sheet_name=None), ignore_index=True)
 # Remove duplicates
 df = panda.DataFrame.drop_duplicates(df)
+# Arrange Alphabetically
+df = df.sort_values('Creators')
 # Convert the dataframe to an XlsxWriter Excel object.
 df.to_excel(writer, sheet_name='SheetClean', index=False)
 # Close the Pandas Excel writer and output the Excel file.
